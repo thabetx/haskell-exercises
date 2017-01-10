11337655b599fc809ae16796f7ac1539ad8fd6c8
Mohamed Thabet <thabetx@gmail.com>
2017-01-11 00:10:34 +0200

    solved run-length-encoding inelegant code

--- run-length-encoding/src/RunLength.hs
+++ run-length-encoding/src/RunLength.hs
@@ -4,17 +4,25 @@ import Data.Char
 
 decode :: String -> String
 decode [] = ""
-decode (x:y:xs)
-          | isNumber x = replicate (read ([x]) ::Int) y ++ decode xs
-          | otherwise =  x : decode (y:xs)
-decode (x:_) = [x]
+decode x = replicate (cnt num_string) next ++ decode (tail rest)
+           where num_string = takeWhile isNumber x
+                 cnt :: String -> Int
+                 cnt s
+                  | s =="" = 1
+                  | otherwise = read s ::Int
+                 rest = dropWhile isNumber x
+                 next = head rest
 
 solve :: Int -> Char -> String -> String
 solve cnt prev (x : xs)
           | prev == x = solve (cnt+1) x xs
-          | otherwise = show(cnt)++[prev]++solve 1 x xs
-solve cnt prev _ = show(cnt)++[prev]
+          | cnt > 1 = show(cnt)++[prev]++solve 1 x xs
+          | otherwise = [prev]++solve 1 x xs
+solve cnt prev _
+          | cnt > 1 = show(cnt)++[prev]
+          | otherwise = [prev]
+
 
 encode :: String -> String
-encode x = filter (/='1')  (drop 2 (solve 1 '?' x))
+encode x = tail (solve 1 '?' x)
 
