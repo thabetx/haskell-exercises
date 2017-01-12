e72d30daef8007e540b35db260130fc199f57472
Mohamed Thabet <thabetx@gmail.com>
2017-01-12 02:59:49 +0200

    cheat sheet update added guards and where syntax

--- cheat-sheet.hs
+++ cheat-sheet.hs
@@ -1,3 +1,14 @@
+{-simple functions-}
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 isEven x = (x`mod`2)==0
+
+{-guards and where-}
+areWeThereYet :: (RealFloat a) => a -> a -> String
+areWeThereYet distance speed
+    | remainingTime <= short = "almost there"
+    | remainingTime <= long  = "not yet"
+    | otherwise              = "..."
+    where remainingTime = distance / speed
+          short = 10
+          long = 1000
