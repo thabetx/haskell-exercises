be34bca9dd6dc36e80ebf642409cccdd76fdfc54
Mohamed Thabet <thabetx@gmail.com>
2017-01-16 00:06:30 +0200

    white space adjustments

--- grains/src/Grains.hs
+++ grains/src/Grains.hs
@@ -2,8 +2,8 @@ module Grains (square, total) where
 
 square :: Integer -> Maybe Integer
 square n
-    | n>0 && n<65 = Just $ 2 ^  (n-1)
+    | n>0 && n<65 = Just $ 2 ^ (n - 1)
     | otherwise = Nothing
 
 total :: Integer
-total = 2^64 -1
+total = 2^64 - 1
