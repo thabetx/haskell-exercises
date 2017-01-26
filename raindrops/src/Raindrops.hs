cba64db6ef23933056d0031c02d77ccd0c206c4b
Mohamed Thabet <thabetx@gmail.com>
2017-01-27 01:07:18 +0200

    use single function for all three cases

--- raindrops/src/Raindrops.hs
+++ raindrops/src/Raindrops.hs
@@ -1,9 +1,7 @@
 module Raindrops (convert) where
 
 convert s
-    | (mod s 3 > 0) && (mod s 5 > 0) && (mod s 7 > 0) = show s
-    | otherwise = x ++ y ++ z
+    | (s `mod` 3 > 0) && (s `mod` 5 > 0) && (s `mod` 7 > 0) = show s
+    | otherwise = add 3 "Pling" ++ add 5 "Plang" ++ add 7 "Plong"
     where
-      x | mod s 3 == 0 = "Pling" | otherwise = ""
-      y | mod s 5 == 0 = "Plang" | otherwise = ""
-      z | mod s 7 == 0 = "Plong" | otherwise = ""
+      add x m | s `mod` x == 0 = m | otherwise = ""
