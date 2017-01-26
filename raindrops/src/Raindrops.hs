9a5fb4124829ce59fc1317e7671008f65507d2b0
Mohamed Thabet <thabetx@gmail.com>
2017-01-27 01:01:19 +0200

    directly use values in where

--- raindrops/src/Raindrops.hs
+++ raindrops/src/Raindrops.hs
@@ -2,8 +2,8 @@ module Raindrops (convert) where
 
 convert s
     | (mod s 3 > 0) && (mod s 5 > 0) && (mod s 7 > 0) = show s
-    | otherwise = x s ++ y s ++ z s
+    | otherwise = x ++ y ++ z
     where
-      x s | mod s 3 == 0 = "Pling" | otherwise = ""
-      y s | mod s 5 == 0 = "Plang" | otherwise = ""
-      z s | mod s 7 == 0 = "Plong" | otherwise = ""
+      x | mod s 3 == 0 = "Pling" | otherwise = ""
+      y | mod s 5 == 0 = "Plang" | otherwise = ""
+      z | mod s 7 == 0 = "Plong" | otherwise = ""
