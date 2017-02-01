bfa420cff609f0f1bdcefb122f2bfe6d732dffb8
Mohamed Thabet <thabetx@gmail.com>
2017-02-01 23:28:03 +0200

    solved Scrabble Score

---
+++ scrabble-score/src/Scrabble.hs
@@ -0,0 +1,18 @@
+module Scrabble (scoreLetter, scoreWord) where
+
+import Data.Char
+scoreLetter :: Char -> Int
+scoreLetter letter
+    | elem x "AEIOULNRST" = 1
+    | elem x "DG"         = 2
+    | elem x "BCMP"       = 3
+    | elem x "FHVWY"      = 4
+    | elem x "K"          = 5
+    | elem x "JX"         = 8
+    | elem x "QZ"         = 10
+    | otherwise           = 0
+    where
+      x = toUpper letter
+
+scoreWord :: String -> Int
+scoreWord word = foldl (\ acc x -> acc + scoreLetter x) 0 word
