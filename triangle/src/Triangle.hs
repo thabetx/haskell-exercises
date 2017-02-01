9798e48eb7441e1f9d39cd934e9fcc29e8852e61
Mohamed Thabet <thabetx@gmail.com>
2017-02-01 04:06:53 +0200

    solved Triangle

---
+++ triangle/src/Triangle.hs
@@ -0,0 +1,21 @@
+module Triangle (TriangleType(..), triangleType) where
+
+import Data.List
+
+data TriangleType = Equilateral
+                  | Isosceles
+                  | Scalene
+                  | Illegal
+                  deriving (Eq, Show)
+
+triangleType :: Float -> Float -> Float -> TriangleType
+triangleType a b c
+    | x+y<z || x==0 = Illegal
+    | x==y && y==z = Equilateral
+    | x==y || y==z = Isosceles
+    | otherwise = Scalene
+    where
+      l = sort [a, b, c]
+      x = l !! 0
+      y = l !! 1
+      z = l !! 2
